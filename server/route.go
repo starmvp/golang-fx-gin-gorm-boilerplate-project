f775b068f0bdb53e150e82406dc84775893bd308
Hauzer S. Lee <nuffin@gmail.com>
2024-11-12 17:00:10 +0800

    chore: refactor routing table register of server app

--- server/route.go
+++ server/route.go
@@ -3,7 +3,7 @@ package server
 import (
 	"fmt"
 	"golang-fx-gin-gorm-boilerplate-project/server/handlers"
-	"golang-fx-gin-gorm-boilerplate-project/server/services"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
@@ -12,22 +12,32 @@ import (
 func (s *AppServer) ConfigureRouteGroups() {
 	fmt.Println("AppServer: Configuring route groups")
 
+	s.Gin.Static("/static/assets", "./webroot/assets")
+
 	s.ApiNoAuth = s.NoAuth.Group("/api/v1/noauth")
 	s.ApiNoAuth.Use(func(c *gin.Context) {
 	})
-	fmt.Println("AppServer: Configuring route groups NoAuth. s.ApiNoAuth: ", s.ApiNoAuth)
+	fmt.Println("AppServer: Configuring route groups ApiNoAuth. s.ApiNoAuth: ", s.ApiNoAuth)
 
 	s.ApiNeedsAuth = s.NeedsAuth.Group("/api/v1/needauth")
 	s.ApiNeedsAuth.Use(func(c *gin.Context) {
 	})
-	fmt.Println("AppServer: Configuring route groups NeedsAuth. s.ApiNeedAuth: ", s.ApiNeedsAuth)
+	fmt.Println("AppServer: Configuring route groups ApiNeedsAuth. s.ApiNeedAuth: ", s.ApiNeedsAuth)
+}
+
+func AppendFallbackRoute() fx.Option {
+	return fx.Invoke(func(s *AppServer) {
+		fmt.Println("Register fallback route")
+		s.Gin.NoRoute(func(c *gin.Context) {
+			if !strings.HasPrefix(c.Request.URL.Path, "/api") {
+				c.File("./webroot/index.html")
+			}
+		})
+	})
 }
 
 var routesModule = fx.Options(
 	fx.Invoke(
-		func(s *services.PingService) {
-			fmt.Println("Server: Configuring services: ping: ", s)
-		},
 		func(s *AppServer, handler *handlers.HealthCheckHandler) {
 			fmt.Println("Server: Configuring routes: health")
 			s.Handlers = append(s.Handlers, handler)
@@ -44,4 +54,6 @@ var routesModule = fx.Options(
 			s.ApiNeedsAuth.GET("/ping", handler.Ping())
 		},
 	),
+	fx.Invoke(func() { fmt.Println("All primary routes have been registered.") }),
+	AppendFallbackRoute(),
 )
